21423f69d1d70d8cea002a147ded8078ff6b240e
jonatas <jhonatas.fender>
2018-01-09 13:22:04 -0200

    my first code in python

--- script-02.py
+++ script-02.py
@@ -1,27 +1,6 @@
-# import re,email,smtplib
-# from lib.xlrd import xlrd
-
-# # msg = email.message_from_string('warning')
-# # msg['From'] = "debora.turibio@hotmail.com"
-# # msg['To'] = "jhonatas.fender@gmail.com"
-# # msg['Subject'] = "helOoooOo"
-
-# msg = '''\
-# From: debora.turibio@hotmail.com
-# To: jhonatas.fender@gmail.com
-# Subject: testin'...
-
-# This is a test '''
-
-# s = smtplib.SMTP("smtp.live.com",587)
-# s.ehlo() 
-# s.starttls() 
-# s.ehlo()
-# s.login('debora.turibio@hotmail.com', 'mecatual2012')
-
-# s.sendmail("debora.turibio@hotmail.com", "jhonatas.fender@gmail.com", msg)
-
-# s.quit()
+# encoding: utf-8
+# encoding: iso-8859-1
+# encoding: win-1252
 
 import email,smtplib,re,os
 from os.path import basename
@@ -31,43 +10,49 @@ from email.mime.text import MIMEText
 from lib.xlrd import xlrd
 
 
+smtp = smtplib.SMTP("smtp.live.com",587)
+smtp.ehlo() 
+smtp.starttls() 
+smtp.ehlo()
+smtp.login('debora.turibio@hotmail.com', 'mecatual2012')
+
 def send_mail(send_from, send_to, subject, text, files=None,server="smtp.live.com"):
     # assert isinstance(send_to, list)
 
     msg = MIMEMultipart()
     msg['From'] = send_from
     msg['To'] = send_to
-    msg['Subject'] = subject
+    msg['Subject'] = unicode(subject, 'utf-8')
 
     msg.attach(MIMEText(text))
 
-    for f in os.walk(files) or []:
-        with open(f, "rb") as fil:
-            part = MIMEApplication(
-                fil.read(),
-                Name=basename(f)
-            )
-        # After the file is closed
-        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
-        msg.attach(part)
+    with open(files, "rb") as fil:
+        part = MIMEApplication(
+            fil.read(),
+            Name=basename(files)
+        )
 
-	smtp = smtplib.SMTP("smtp.live.com",587)
-	smtp.ehlo() 
-	smtp.starttls() 
-	smtp.ehlo()
-	smtp.login('debora.turibio@hotmail.com', 'mecatual2012')
+    part['Content-Disposition'] = 'attachment; filename="%s"' % basename(files)
+    msg.attach(part)
+
+
     smtp.sendmail(send_from, send_to, msg.as_string())
-    smtp.quit()
+    # smtp.quit()
 
 w = xlrd.open_workbook("/home/jonatas/Desktop/email.xlsx")
 s = w.sheet_by_index(0);
-a = list();
+
 for r in range(s.nrows):
 	c = s.row_values(r)
 
 	if(re.match("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",c[1])):
 		print(c[1])
-		a.append(c[1])
-		send_mail("debora.turibio@hotmail.com",c[1],"Curriculo Vitale","Boa noite ou bom dia","/home/jonatas/Downloads/debora.pdf")
+		send_mail(
+			"debora.turibio@hotmail.com",
+			c[1],
+			"Currículo estudante de enfermagem",
+			'Sou Débora de Andrade Turíbio, estudante de enfermagem cursando o quinto semestre. Desejo adquirir experiência e oportunidade para poder entrar no mercado de trabalho da saúde, alcançando os objetivos profissionais.',
+			"./debora.pdf"
+		)
 
 
